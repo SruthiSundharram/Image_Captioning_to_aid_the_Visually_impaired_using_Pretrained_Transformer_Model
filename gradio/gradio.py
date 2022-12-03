7acb9de48a722a974a8bd7efe3fc6623151d7c8a
SruthiSundharram <sruthikvs99@gmail.com>
2022-12-03 00:43:45 -0500

    Changed Image to speech function

--- gradio/gradio.py
+++ gradio/gradio.py
@@ -6,22 +6,27 @@ from pydub import AudioSegment
 from pydub.playback import play
 import os
 
+# load the model
 model = pipeline('image-to-text')
 
+# defining the image to speech function
 def img2Speech(image):
   description = model(image)
   generated_text = description[0]['generated_text']
   generated_audio = gTTS(generated_text)
   generated_audio.save("demo.mp3")
-  song = AudioSegment.from_mp3("demo.mp3")
-  play(song)
-  return play(song)
+  return 'demo.mp3'
 
+# design the interface using gradio
 demo = gr.Interface(
+    # call img to speech conversion function
     fn = img2Speech,
-    source=gr.Image()
-    outputs=gr.Audio(),
+    # specify the input format
+    inputs = gr.Image(source="webcam", type='pil'),
+    # specify the output format
+    outputs=gr.Audio()
 
 )
 
+# launch the application
 demo.launch(debug=True)
