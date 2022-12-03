f45a7e58e21b1ea9389f0772bca322081fbb9cbb
Aushij Singh <55728404+Aushij@users.noreply.github.com>
2022-12-03 01:09:24 -0500

    Update gradio.py

--- gradio/gradio.py
+++ gradio/gradio.py
@@ -9,18 +9,22 @@ import os
 model = pipeline('image-to-text')
 
 def img2Speech(image):
+  #image = Image(image)
   description = model(image)
   generated_text = description[0]['generated_text']
   generated_audio = gTTS(generated_text)
   generated_audio.save("demo.mp3")
-  song = AudioSegment.from_mp3("demo.mp3")
-  play(song)
-  return play(song)
+
+  #song = AudioSegment.from_mp3("demo.mp3")
+  #play(song)
+  #os.system("demo.mp3")
+  #return play(song)
+  return 'demo.mp3'
 
 demo = gr.Interface(
     fn = img2Speech,
-    source=gr.Image()
-    outputs=gr.Audio(),
+    inputs = gr.Image(), #source="webcam", type='pil'), #source="webcam", source='upload', type='pil'
+    outputs=gr.Audio() # gr.Audio(),
 
 )
 
